8a29e1994e7a103a77d2e048b5b174b40bba7c5d
wangdiwen <dw_wang126@126.com>
2013-09-13 13:57:16 +0800

    clear log file, by diwen

--- application/frontend/logs/log-2013-08-26.php
+++
@@ -1,6 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
-
-ERROR - 2013-08-26 22:05:09 --> 删除临时pdf文件，失败
-ERROR - 2013-08-26 22:20:22 --> Severity: Notice  --> Undefined offset:  1 /alidata/www/creamnote/application/frontend/controllers/core/wxc_user_manager.php 157
-ERROR - 2013-08-26 23:23:35 --> 404 Page Not Found --> primary/wxc_home
-ERROR - 2013-08-26 23:47:38 --> 删除临时pdf文件，成功
