25725a7d0c227a07b1bfeaa12744ec655a38641e
wangdiwen <dw_wang126@126.com>
2013-09-19 20:22:13 +0800

    del logs file, by diwen

--- application/frontend/logs/log-2013-09-17.php
+++
@@ -1,9 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
-
-ERROR - 2013-09-17 00:01:39 --> 生成PDF文件
-ERROR - 2013-09-17 00:01:39 --> holy
-ERROR - 2013-09-17 00:01:41 --> shit
-ERROR - 2013-09-17 23:56:16 --> Severity: Notice  --> Trying to get property of non-object /alidata/www/creamnote/application/frontend/controllers/primary/wxc_search.php 49
-ERROR - 2013-09-17 23:56:44 --> Severity: Notice  --> Trying to get property of non-object /alidata/www/creamnote/application/frontend/controllers/primary/wxc_search.php 49
-ERROR - 2013-09-17 23:56:44 --> Severity: Notice  --> Trying to get property of non-object /alidata/www/creamnote/application/frontend/controllers/primary/wxc_search.php 49
-ERROR - 2013-09-17 23:59:01 --> Severity: Notice  --> Undefined variable: file_exists /alidata/www/creamnote/application/frontend/controllers/data/wxc_data.php 650
