4cffdc86cd97e53fb74c0cd5afb52fac3d87061d
wangdiwen <dw_wang126@126.com>
2013-11-01 15:50:13 +0800

    creamnote beta online, by diwen

--- application/frontend/config/database.php
+++ application/frontend/config/database.php
@@ -48,10 +48,28 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
-$db['default']['password'] = 'wx@creamnote';
-$db['default']['database'] = 'CREAMNOTE';
+/***************** 本地数据库配置 ***************/
+// $db['default']['hostname'] = 'localhost';
+// $db['default']['username'] = 'root';
+// $db['default']['password'] = 'wx@creamnote';
+// $db['default']['database'] = 'CREAMNOTE';
+// $db['default']['dbdriver'] = 'mysql';
+// $db['default']['dbprefix'] = '';
+// $db['default']['pconnect'] = TRUE;
+// $db['default']['db_debug'] = TRUE;
+// $db['default']['cache_on'] = FALSE;
+// $db['default']['cachedir'] = '';
+// $db['default']['char_set'] = 'utf8';
+// $db['default']['dbcollat'] = 'utf8_general_ci';
+// $db['default']['swap_pre'] = '';
+// $db['default']['autoinit'] = TRUE;
+// $db['default']['stricton'] = FALSE;
+
+/************* 阿里RDS 数据库配置 ******************/
+$db['default']['hostname'] = 'rdsnyamvqnyamvq.mysql.rds.aliyuncs.com';
+$db['default']['username'] = 'wangdiwen';
+$db['default']['password'] = 'wx2creamnote';
+$db['default']['database'] = 'creamnote';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
@@ -64,6 +82,7 @@ $db['default']['swap_pre'] = '';
 $db['default']['autoinit'] = TRUE;
 $db['default']['stricton'] = FALSE;
 
+/**************************************************/
 
 /* End of file database.php */
 /* Location: ./application/config/database.php */
