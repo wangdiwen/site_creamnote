a706629b225029d9e1c13d4b2caf736a0f5500fb
wangdiwen <dw_wang126@126.com>
2013-11-23 13:46:19 +0800

    revise auto task, set php script exec timeout, by diwen

--- auto_manager/model/wx_log_api.php
+++ auto_manager/model/wx_log_api.php
@@ -12,8 +12,11 @@ if (! function_exists('wx_log'))
         if (WX_LOG) {
             if (defined('WX_LOG_PATH')) {
                 $log_path = WX_LOG_PATH;
-                if (empty($log_path) || !file_exists($log_path)) {
-                    throw new Exception('Log Error: '.$log_path.' not exist');
+                if (empty($log_path) || ! is_dir($log_path)) {
+                    // throw new Exception('Log Error: '.$log_path.' not exist');
+                    $error_log_file = '/alidata/www/creamnote/auto_manager/log/log_exception_'.date('Y-m-d').'.log';
+                    error_log('['.date('Y-m-d H:i:s').'] : Log Exception '.$log_file_name." \n", 3, $error_log_file);
+                    return false;
                 }
 
                 $log_file = $log_path.WX_SEPARATOR.'auto_'.$log_file_name.'_'.date('Y-m-d').'.log';
@@ -27,9 +30,11 @@ if (! function_exists('wx_log'))
 					// if log exception, record to a single log file
 					$except_log_file = $log_path.WX_SEPARATOR.'log_exception_'.date('Y-m-d').'.log';
 					error_log('['.date('Y-m-d H:i:s').'] : Log Exception '.$log_file_name." \n", 3, $except_log_file);
+                    return false;
                 }
             }
         }
+        return true;
     }
 }
 
