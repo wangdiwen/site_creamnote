cd699ad5da82fa5e8e3460b2ab51fd9d03e7e9e7
wangdiwen <dw_wang126@126.com>
2014-01-14 23:25:10 +0800

    modify weixin iface, by diwen

--- application/weixin/controllers/home.php
+++ application/weixin/controllers/home.php
@@ -12,93 +12,29 @@ class Home extends CI_Controller {
 /*****************************************************************************/
     public function digest() {      // 微信订阅号，入口，用于接收用户信息
         $xml_content = file_get_contents('php://input');
-        // wx_loginfo('weixin digest user msg ...');
-        // wx_loginfo($xml_content);
 
         if ($xml_content) {
-            // wx_loginfo($xml_content);
             $xml_obj = simplexml_load_string($xml_content, 'SimpleXMLElement', LIBXML_NOCDATA);
-            $from_user_name = $xml_obj->FromUserName;
-            $to_user_name = $xml_obj->ToUserName;
-            $keyword = trim($xml_obj->Content);
+            $user_name = $xml_obj->FromUserName;
+            $my_name = $xml_obj->ToUserName;
+            $msg_type = isset($xml_obj->MsgType) ? $xml_obj->MsgType : '';
+            $keyword = isset($xml_obj->Content) ? trim($xml_obj->Content) : '';
 
-            // convert the user's msg character to utf-8
-            $keyword = mb_convert_encoding($keyword, 'UTF-8');
-            wx_loginfo('user msg => '.$keyword);
-
-            // here, handle user's msg, based by $keyword
-            $respose_text = "<xml>
-                        <ToUserName><![CDATA[%s]]></ToUserName>
-                        <FromUserName><![CDATA[%s]]></FromUserName>
-                        <CreateTime>%s</CreateTime>
-                        <MsgType><![CDATA[%s]]></MsgType>
-                        <Content><![CDATA[%s]]></Content>
-                        <FuncFlag>0</FuncFlag>
-                        </xml>";
             $respose = '';
-            if ($keyword) {
-                // process the keyword via $keyword
-                // 定义处理的关键词（英文、中文单词，比如：help，帮助），判断下一步需要处理的标记 flag，
-                // 关键词标记及功能，如下：
-                // '001' => 欢迎致辞
-                // '002' => 帮助
-                // '003' => 一则世界超短迷你小说
-                $process_flag = '001';
-                switch ($keyword){
-                    case 'hello':
-                    case '你好':
-                        $process_flag = '001';
-                        break;
-                    case 'help':
-                    case '帮助':
-                        $process_flag = '002';
-                        break;
-                    case 'story':
-                    case '故事':
-                        $process_flag = '003';
-                        break;
-                    case 'note':
-                    case '笔记':
-                        $process_flag = '004';
-                        break;
+            if ($msg_type == 'text') {
+                // convert the user's msg character to utf-8
+                $keyword = mb_convert_encoding($keyword, 'UTF-8');
+                if ($keyword) {
+                    $process_flag = $this->_filter_keyword($keyword);
+                    $respose = $this->_weixin_basefunc($process_flag, $user_name, $my_name); // 3 base func
+                }
+            }
+            elseif ($msg_type == 'event') {
+                $event_content = isset($xml_obj->Event) ? $xml_obj->Event : '';  // 事件类型，subscribe(订阅)、unsubscribe(取消订阅)
+                $respose = $this->_weixin_event($event_content, $user_name, $my_name);
+            }
 
-                    default:
-                        break;
-                }
-
-                $msg_type = 'text';
-                $msg_time = time();
-                $msg_user = "";
-                $msg_file = 'application/weixin/weixin_digest/digest_auto_';
-
-                if ($process_flag === '001') {
-                    $msg_file .= $process_flag;
-                    if (file_exists($msg_file)) {
-                        $msg_user = file_get_contents($msg_file);
-                    }
-                    $respose = sprintf($respose_text, $from_user_name, $to_user_name, $msg_time, $msg_type, $msg_user);
-                }
-                elseif ($process_flag === '002') {
-                    $msg_file .= $process_flag;
-                    if (file_exists($msg_file)) {
-                        $msg_user = file_get_contents($msg_file);
-                    }
-                    $respose = sprintf($respose_text, $from_user_name, $to_user_name, $msg_time, $msg_type, $msg_user);
-                }
-                elseif ($process_flag === '003') {
-                    $msg_file .= $process_flag;
-                    if (file_exists($msg_file)) {
-                        $msg_user = file_get_contents($msg_file);
-                    }
-                    $respose = sprintf($respose_text, $from_user_name, $to_user_name, $msg_time, $msg_type, $msg_user);
-                }
-                elseif ($process_flag === '004') {
-                    // todo ...
-                    $msg_user = "Creamnote.com 醍醐笔记网\n为您推荐精品“学霸笔记” ~";
-                    $respose = sprintf($respose_text, $from_user_name, $to_user_name, $msg_time, $msg_type, $msg_user);
-                }
-
-                // respose msg to user
+            if ($respose) {
                 echo $respose;
             }
         }
@@ -106,6 +42,99 @@ class Home extends CI_Controller {
             echo '';
             exit();
         }
+    }
+/*****************************************************************************/
+/*****************************************************************************/
+/*****************************************************************************/
+    private function _weixin_event($event = '', $user_name = '', $my_name = '') {
+        $respose_text = "<xml>
+                        <ToUserName><![CDATA[%s]]></ToUserName>
+                        <FromUserName><![CDATA[%s]]></FromUserName>
+                        <CreateTime>%s</CreateTime>
+                        <MsgType><![CDATA[%s]]></MsgType>
+                        <Content><![CDATA[%s]]></Content>
+                        <FuncFlag>0</FuncFlag>
+                        </xml>";
+        $msg_time = time();
+        $msg_type = 'text';
+        $msg_user = '';
+        $msg_file = 'application/weixin/weixin_digest/digest_auto_001';
+        if ($event == 'subscribe' || $event == 'unsubscribe') {
+            if (file_exists($msg_file)) {
+                $msg_user = file_get_contents($msg_file);
+            }
+        }
+        if ($msg_user && $user_name && $my_name) {
+            return sprintf($respose_text, $user_name, $my_name, $msg_time, $msg_type, $msg_user);
+        }
+        return '';
+    }
+/*****************************************************************************/
+    private function _weixin_basefunc($process_flag = '001', $user_name = '', $my_name = '') {
+        $respose_text = "<xml>
+                        <ToUserName><![CDATA[%s]]></ToUserName>
+                        <FromUserName><![CDATA[%s]]></FromUserName>
+                        <CreateTime>%s</CreateTime>
+                        <MsgType><![CDATA[%s]]></MsgType>
+                        <Content><![CDATA[%s]]></Content>
+                        <FuncFlag>0</FuncFlag>
+                        </xml>";
+        $msg_time = time();
+        $msg_type = 'text';
+        $msg_user = '';
+        $msg_file = 'application/weixin/weixin_digest/digest_auto_';
+
+        if ($process_flag == '001') {
+            $msg_file .= $process_flag;
+            if (file_exists($msg_file)) {
+                $msg_user = file_get_contents($msg_file);
+            }
+        }
+        elseif ($process_flag == '002') {
+            $msg_file .= $process_flag;
+            if (file_exists($msg_file)) {
+                $msg_user = file_get_contents($msg_file);
+            }
+        }
+        elseif ($process_flag == '003') {
+            $msg_file .= $process_flag;
+            if (file_exists($msg_file)) {
+                $msg_user = file_get_contents($msg_file);
+            }
+        }
+
+        if ($msg_user) {
+            return sprintf($respose_text, $user_name, $my_name, $msg_time, $msg_type, $msg_user);
+        }
+        return '';
+    }
+/*****************************************************************************/
+    private function _filter_keyword($keyword = '') {
+        // 定义处理的关键词（英文、中文单词，比如：help，帮助），判断下一步需要处理的标记 flag，
+        // 关键词标记及功能，如下：
+        // '001' => 欢迎致辞
+        // '002' => 帮助
+        // '003' => 一则世界超短迷你小说
+        $flag = '001';
+        if ($keyword) {
+            switch ($keyword) {
+                case 'hello':
+                case '你好':
+                    $flag = '001';
+                    break;
+                case 'help':
+                case '帮助':
+                    $flag = '002';
+                    break;
+                case 'story':
+                case '故事':
+                    $flag = '003';
+                    break;
+                default:
+                    break;
+            }
+        }
+        return $flag;
     }
 /*****************************************************************************/
     public function digest_check() {    // 订阅号，开发者验证
