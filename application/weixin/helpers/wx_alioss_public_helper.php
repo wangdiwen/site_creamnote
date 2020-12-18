1f8a2d24c0b247f884e10511681532aa6aa652c3
Steven Wang <dw_wang126@126.com>
2020-12-18 14:48:19 +0800

    Update wx_alioss_public_helper.php
    del key info

--- application/weixin/helpers/wx_alioss_public_helper.php
+++ application/weixin/helpers/wx_alioss_public_helper.php
@@ -22,8 +22,8 @@
      *  说明：官方php开发包sdk文件为：conf.inc.php
      */
 /*****************************************************************************/
-    define('OSS_ACCESS_ID', 'DvOTloljBCJo84zj');              //ACCESS_ID
-    define('OSS_ACCESS_KEY', '3YoDhKaoeBxp9jiWXbkvrBSedJm5sB');             //ACCESS_KEY
+    define('OSS_ACCESS_ID', '');              //ACCESS_ID
+    define('OSS_ACCESS_KEY', '');             //ACCESS_KEY
     define('ALI_LOG', FALSE);                       //是否记录日志
     define('ALI_LOG_PATH','../logs');               //自定义日志路径，如果没有设置，则使用系统默认路径，在./logs/
     define('ALI_DISPLAY_LOG', FALSE);               //是否显示LOG输出
